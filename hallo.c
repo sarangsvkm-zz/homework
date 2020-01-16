b596783e3802db5c7981f9032f023ac97b27cec7
Sarang <44294204+sarangsvkm@users.noreply.github.com>
2020-01-16 21:17:42 +0530

    Add files via upload

---
+++ hallo.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : sample2.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("!!!Hello World!!!$sarang"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
